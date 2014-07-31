8d6d4de7aff7d225cbedf90c8fe90ceb44e9e4f1
yankuangshi <yankuangshi@gmail.com>
2014-07-31 23:59:31 +0200

    add files

---
+++ gotask.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import gflags
+import httplib2
+
+from apiclient.discovery import build
+from oauth2client.file import Storage
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.tools import run
+
+class GoTask:
+
+    def __init__(self):
+        
+        FLAGS = gflags.FLAGS
+
+        # Set up a Flow object to be used if we need to authenticate. This
+        # sample uses OAuth 2.0, and we set up the OAuth2WebServerFlow with
+        # the information it needs to authenticate. Note that it is called
+        # the Web Server Flow, but it can also handle the flow for native
+        # applications
+        # The client_id and client_secret are copied from the API Access tab on
+        # the Google APIs Console
+        FLOW = OAuth2WebServerFlow(
+        client_id='350392034727-p663o1j04sepb3ik4sp67u384o1jjee6.apps.googleusercontent.com',
+        client_secret='o8V6_dLLJ-S5QKKxidE_Z0Yg',
+        scope='https://www.googleapis.com/auth/tasks',
+        user_agent='google-task-ui/v1')
+
+        # To disable the local server feature, uncomment the following line:
+        FLAGS.auth_local_webserver = False
+
+        # If the Credentials don't exist or are invalid, run through the native client
+        # flow. The Storage object will ensure that if successful the good
+        # Credentials will get written back to a file.
+        storage = Storage('tasks.dat')
+        credentials = storage.get()
+        if credentials is None or credentials.invalid == True:
+            credentials = run(FLOW, storage)
+
+        # Create an httplib2.Http object to handle our HTTP requests and authorize it
+        # with our good Credentials.
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+
+        # Build a service object for interacting with the API. Visit
+        # the Google APIs Console
+        # to get a developerKey for your own application.
+        self.service = build(serviceName='tasks', version='v1', 
+                        http=http, developerKey='AIzaSyAwrNJT65OhdkwuxWEdh-ZUPP3kkPOk804')
+
+    def list_tasklists(self):
+        """List an user's all tasklists
+
+        """
+        tasklists = self.service.tasklists().list().execute()
+        return tasklists['items']
+
+#if __name__ == '__main__':
+#    gotask = GoTask()
+#    gotask.list_tasklists()
--- ui.py
+++ ui.py
@@ -3,6 +3,8 @@
 
 import curses
 
+from gotask import GoTask
+
 class Ui:
 
     def __init__(self):
@@ -12,5 +14,47 @@ class Ui:
         curses.start_color()
         curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
         self.screen.keypad(1)
+    
+    def build_tasklists(self):
+        """Build tasklists menu
 
+        """
+        gotask = GoTask()
+        tasklists = gotask.list_tasklists()
+        num_tasklists = len(tasklists)
+        opt = 0
+        step = 1
+        offset = 4
+        select = -1
+        while select != num_tasklists:
+            self.screen.clear()
+            self.screen.addstr(2, 4, "Term - Google Task UI")
+            self.screen.addstr(offset, 4, "Please select one tasklist for more details...", curses.A_BOLD)
+            for i in range(num_tasklists + 1):
+                if i < num_tasklists:
+                    if i == opt:
+                        self.screen.addstr(offset + step + i, 4, '-> ' + str(i+1) + '. ' + tasklists[i]['title'], curses.color_pair(1))
+                    else:
+                        self.screen.addstr(offset + step + i, 7, str(i+1) + '. ' + tasklists[i]['title'])
+                else:
+                    if i == opt:
+                        self.screen.addstr(offset + step + i, 4, "-> Exit ('q')", curses.color_pair(1))
+                    else:
+                        self.screen.addstr(offset + step + i, 7, "Exit ('q')")
+                    
+            self.screen.refresh()
+            q = self.screen.getch()
+            if q == curses.KEY_UP or q == ord('k'):     # KEY_UP or 'k' on vi/vim mode
+                opt = (opt - 1) % (num_tasklists + 1)
+            elif q == curses.KEY_DOWN or q == ord('j'): # KEY_DOWN or 'j' on vi/vim mode
+                opt = (opt + 1) % (num_tasklists + 1)
+            elif q == ord('\n'):
+                select = opt
+            if q == ord('q') or select == num_tasklists:
+                break
 
+        curses.endwin()
+        
+if __name__ == '__main__':
+    ui = Ui()
+    ui.build_tasklists()
